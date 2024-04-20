dafbc390ac032f3605e9d0e028000d19aee08b54
Sulik Szabolcs <sulik.szabolcs@gmail.com>
2024-04-20 21:51:41 +0200

    use catppuccin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,30 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = function()
+      --vim.cmd.colorscheme("catppuccin-latte")
+      --vim.cmd.colorscheme("catppuccin-frappe")
+      --vim.cmd.colorscheme("catppuccin-macchiato")
+      require('catppuccin').setup {
+        flavour = 'mocha',
+        integrations = {
+          cmp = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          neotest = true,
+          dap = true,
+          mason = true,
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+}
