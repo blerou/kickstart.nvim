bce1389e18a6cfa196a3cd35eeaea5d07def1726
Sulik Szabolcs <sulik.szabolcs@gmail.com>
2024-04-30 23:42:41 +0200

    update

--- init.lua
+++ init.lua
@@ -201,8 +201,10 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 
 vim.keymap.set('x', '<leader>p', '"_dP')
 
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+Y')
+-- "+ on linux
+-- "* on macos
+vim.keymap.set('n', '<leader>y', '"*y')
+vim.keymap.set('n', '<leader>Y', '"*Y')
 
 vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 
