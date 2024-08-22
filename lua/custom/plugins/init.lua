65906fe65e514251fa97624d446c9e70f1061b36
Sulik Szabolcs <sulik.szabolcs@gmail.com>
2024-08-22 19:18:39 +0200

    cleanup and impros

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,15 +1,8 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
     config = function()
-      --vim.cmd.colorscheme("catppuccin-latte")
-      --vim.cmd.colorscheme("catppuccin-frappe")
-      --vim.cmd.colorscheme("catppuccin-macchiato")
       require('catppuccin').setup {
         flavour = 'mocha',
         integrations = {
@@ -29,32 +22,25 @@ return {
     end,
   },
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      -- vim.cmd.colorscheme 'rose-pine'
-    end,
-  },
-  {
     'scalameta/nvim-metals',
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
     ft = { 'scala', 'sbt', 'java' },
     opts = function()
-      local metals_config = require('metals').bare_config()
-      metals_config.on_attach = function(client, bufnr)
+      local metals = require('metals').bare_config()
+      metals.on_attach = function(client, bufnr)
         -- your on_attach function
       end
 
-      return metals_config
+      return metals
     end,
-    config = function(self, metals_config)
+    config = function(self, metals)
       local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
       vim.api.nvim_create_autocmd('FileType', {
         pattern = self.ft,
         callback = function()
-          require('metals').initialize_or_attach(metals_config)
+          require('metals').initialize_or_attach(metals)
         end,
         group = nvim_metals_group,
       })
