01f29a839b3ca714ddd0f28b3165eac829c8512c
Sulik Szabolcs <sulik.szabolcs@gmail.com>
2024-08-23 10:04:59 +0200

    session save on exit

--- init.lua
+++ init.lua
@@ -227,6 +227,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('ExitPre', {
+  desc = 'Session save on Quit',
+  callback = function()
+    vim.cmd 'mks!'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
